e43444fee909f9449e16cf55572bc87b819aa17a
dangnq2501 <nguyenquydang251@gmail.com>
2025-02-21 12:00:53 +0700

    update

--- test_gtc.m
+++
@@ -1,8 +0,0 @@
-load('/MATLAB Drive/GTC/L_PeMS08.mat');
-%load('/MATLAB Drive/GTC/L_PeMS04.mat')
-figure;
-imagesc(L); % Hiển thị dữ liệu traffic (ma trận thời gian - không gian)
-colorbar;
-title('Traffic Volume Data (Original)');
-xlabel('Time');
-ylabel('Sensor Locations');
